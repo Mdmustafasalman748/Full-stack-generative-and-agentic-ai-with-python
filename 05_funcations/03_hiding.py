a5159303cc05373a23cf9bd5f78e78bdf800392f
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-06-25 17:23:07 -0400

    Initial commit

---
+++ 05_funcations/03_hiding.py
@@ -0,0 +1,12 @@
+def get_input():
+    print("Getting user input")
+def validate_input():
+    print("Validating the user info")
+def save_to_db():
+    print("Saving to database")
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("User registered successfully")
+register_user()
