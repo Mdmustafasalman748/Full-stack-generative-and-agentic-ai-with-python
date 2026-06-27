60a525ed69734be2d40a44ff8c0745fa7298d901
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-06-27 10:26:46 -0400

    Initial commit

---
+++ 05_functions/06_scopes.py
@@ -0,0 +1,20 @@
+def serve_chai():
+    chai_type="Masala" #local scope
+    print(f"Inside function {chai_type}")
+
+chai_type="Lemon"
+serve_chai()
+print(f"Outside function {chai_type}")
+
+def chai_counter():
+    chai_order="lemon" #enclosing scope
+    def print_order():
+        chai_order="ginger" #local scope
+        print("Inner:",chai_order)
+    print_order()
+    print("Outer:",chai_order)
+    
+chai_order="Tulsi" #global scope
+chai_counter()
+print("Global:",chai_order)
+    
