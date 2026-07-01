a82df5a0cecbd5842cf380eda9c4b21e627de07c
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-07-01 13:25:10 -0400

    Initial commit

---
+++ 08_generators.py/03_send_generators.py
@@ -0,0 +1,10 @@
+def chai_customer():
+    print("Welcome! What chai would you like?")
+    order=yield
+    while True:
+        print(f"Preparing your {order}...")
+        order=yield
+stall=chai_customer()
+next(stall)
+stall.send("Masala chai")
+stall.send("Lemon chai")
