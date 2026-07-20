6789d640d144deee940579ab219ce8a77373c2a0
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-07-20 17:38:57 -0400

    Initial commit

--- LLM/hello_world/gemini_hello.py
+++ LLM/hello_world/gemini_hello.py
@@ -1,6 +1,16 @@
 from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 client = genai.Client(
-    api_key="GEMINI_API_KEY"
+    api_key=os.getenv("GEMINI_API_KEY")
 )
-response=client.models.generate_content(model="gemini-3.5-flash", contents="Explain how AI works in a few words")
-print (response.text)
+
+response = client.models.generate_content(
+    model="gemini-3.5-flash",
+    contents="Hey, I am MS! Nice to meet you. Who are you?"
+)
+
+print(response.text)
--- LLM/hello_world/gemini_openai.py
+++ LLM/hello_world/gemini_openai.py
@@ -1,9 +1,11 @@
+import os
 from dotenv import load_dotenv
 from openai import OpenAI
 load_dotenv()
 client = OpenAI(
-api_key="GEMINI_API_KEY",
-base_url="https://generativelanguage.googleapis.com/v1beta/"
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
+
 response = client.chat.completions.create(model="gemini-3.5-flash", messages=[{"role": "user", "content": "Hey, I am MS! Nice to meet you. Who are you?"}])
 print(response.choices[0].message.content)
