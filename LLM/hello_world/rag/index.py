5f160a579e58ec4b4384ad135fa7ace4e5f02a45
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-07-29 00:07:56 -0400

    Initial commit

---
+++ LLM/hello_world/rag/index.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+pdf_path=Path(__file__).parent/"nodejs.pdf"
+#Load this file in python program
+loader=PyPDFLoader(file_path=pdf_path)
+docs=loader.load()
+print(docs[12])
