edb29fbb18f7cd4942ee0acb71ba70515830dc83
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-07-30 23:36:05 -0400

    Initial commit

--- LLM/hello_world/rag_queue/server.py
+++ LLM/hello_world/rag_queue/server.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from .client.rq_client import queue
+from .queues.worker import process_query
+from dotenv import load_dotenv
+load_dotenv()
 app=FastAPI()
 @app.get("/")
 def root():
     return {"Status":'Server is up and running'}
+@app.post("/chat")
+def chat(
+    query:str=Query(...,description="The chat query of user")
+):
+    job=queue.enqueue_call(process_query,query)
+    return {"Status":"Queued","job_id":job.id}
