4e58927ba240dfecea43f66a6d8bd275c5d8f27b
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-08-04 10:44:52 -0400

    Initial commit

--- LLM/hello_world/langgraph_learn/chat.py
+++ LLM/hello_world/langgraph_learn/chat.py
@@ -4,4 +4,18 @@ from langgraph.graph.message import add_message
 from langgraph.graph import StateGraph
 class State(TypeDict):
     messages:Annotated[list, add_message]
+    
+def chatbot(state:State):
+    print("\n\n Inside chatbot node",state)
+    return {"messages":["Hi, This is a message from chatbot node"]}
+
+def samplenode(state:State):
+    print("\n\n Inside samplenode",state)
+    return {"messages":["Sample Message Appended"]}
 graph_builder=StateGraph(State)
+graph_builder.add_node("chatbot",chatbot)
+graph_builder.add_node("samplenode",samplenode)
+
+#state={"messages":["Hey There"]}
+#node runs: chatbot(state:["Hey There"]) -> ["Hi, This is a message from chatbot node"]
+#state={"messages":["Hey There","Hi, This is a message from chatbot node"]}
