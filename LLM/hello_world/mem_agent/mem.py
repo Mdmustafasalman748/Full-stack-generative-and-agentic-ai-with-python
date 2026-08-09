9f509a9698dac2345d8f45417a12d78e3fa3ee01
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-08-08 20:50:11 -0400

    Initial commit

--- LLM/hello_world/mem_agent/mem.py
+++ LLM/hello_world/mem_agent/mem.py
@@ -1,15 +1,20 @@
+from dotenv import load_dotenv
+from openai import OpenAI
 from mem0 import Memory
 import os 
-OPEN_AI_KEY = os.getenv("OPENAI_API_KEY")
+
+load_dotenv()
+client = OpenAI()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 config={
     "version":"v1.1",
     "embedder":{
         "provider":"openai",
-        "config":{"api_key":OPENAI_API_KEY,"model":"text-embedding-3-small"}
+        "config":{"api_key": OPENAI_API_KEY, "model":"text-embedding-3-small"}
     },
     "llm":{
         "provider":"openai",
-        "config":{"api_key":OPENAI_API_KEY,"model":"gpt-4.1"}
+        "config":{"api_key": OPENAI_API_KEY, "model":"gpt-4.1"}
     },
     "vector_store":{
         "provider":"qdrant",
@@ -20,3 +25,21 @@ config={
     }
 }
 mem_client=Memory.from_config(config)
+while(True):
+    user_query=input(">")
+    response=client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role":"user","content":user_query}
+        ]
+    )
+    ai_response=response.choices[0].message.content
+    print(f"AI: {ai_response}")
+    mem_client.add(user_id="MS",
+                   messages=[
+                       {"role":"user","content":user_query},
+                       {"role":"assistant","content":ai_response}  
+                   ]
+    )
+    print("Memory has been saved...")
+    
