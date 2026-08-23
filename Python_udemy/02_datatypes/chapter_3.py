376a74f8d63ac2063eab7de28c388928afcc5234
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-08-22 22:18:10 -0400

    Initial commit

---
+++ Python_udemy/02_datatypes/chapter_3.py
@@ -0,0 +1,25 @@
+#Integer
+black_tea_grams=14
+ginger_grams=5
+total_grams=black_tea_grams+ginger_grams
+print(f"Total grams of base tea: {total_grams}")
+remaining_tea=black_tea_grams-ginger_grams
+print(f"Total grams of remaining tea: {remaining_tea}") 
+milk_litres=7
+servings=4
+milk_per_serving=milk_litres/servings
+print(f"Milk per serving: {milk_per_serving}")
+total_tea_ags=7
+pots=4
+bags_per_pot=total_tea_ags/pots
+print(f"Tea bags per pot: {bags_per_pot}")
+total_cadamom_pods=10
+pods_per_cup=3
+leftover_pods=total_cadamom_pods%pods_per_cup
+print(f"Leftover cardamom pods: {leftover_pods}")
+base_flavour_strength=2
+scale_factor=3
+powerful_flavour=base_flavour_strength**scale_factor
+print(f"Scaled flavour strength: {powerful_flavour}")
+total_tea_leaves_harvested=1_000_000_000
+print(f"Total tea leaves harvested: {total_tea_leaves_harvested}")
