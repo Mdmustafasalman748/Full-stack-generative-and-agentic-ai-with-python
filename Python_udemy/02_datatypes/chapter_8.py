032586c715fd3a012c78a40f42de251e2341b676
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-08-23 22:20:48 -0400

    Initial main

--- Python_udemy/02_datatypes/chapter_8.py
+++ Python_udemy/02_datatypes/chapter_8.py
@@ -20,3 +20,15 @@ print(f"Chai ingredients sorted: {chai_ingredients}")
 sugar_levels=[1,2,3,4,5]
 print(f"Maximum sugar level: {max(sugar_levels)}")
 print(f"Minimum sugar level: {min(sugar_levels)}")
+
+base_liquid=["water","milk"]
+extra_flavor=["ginger"]
+full_liquid_mix=base_liquid+extra_flavor #operator overloading
+print(f"Full liquid mix: {full_liquid_mix}")
+strong_brew=["black tea", "water"]*3 
+print(f"Strong brew: {strong_brew}")
+
+#To treat each element as list array
+raw_spice_data=bytearray(b"CINNAMON")
+raw_spice_data=raw_spice_data.replace(b"CINNA",b"CARD")
+print(f"Bytes: {raw_spice_data}")
