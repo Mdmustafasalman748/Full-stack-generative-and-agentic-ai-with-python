2c2e88de836a3dd211d57239a3ce2f68f39457e0
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-08-29 12:22:23 -0400

    Initial main

--- Python_udemy/05_functions/01_duplication.py
+++ Python_udemy/05_functions/01_duplication.py
@@ -1,6 +1,25 @@
+#Reducing the duplication of code 
 def print_order(name,chai_type):
     print(f"{name} ordered {chai_type} chai")
 
 print_order("Aman","Masala")
 print_order("Bob","Ginger")
 print_order("Charlie","Tulsi")
+
+#Splitting comples tasks
+def fetch_sales():
+    print("Fetching sales data")
+
+def filter_valid_sales():
+    print("Filtering valid sales")
+
+def summarize_data():
+    print("Summarizing data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+    print("Report is ready")
+    
+generate_report()
