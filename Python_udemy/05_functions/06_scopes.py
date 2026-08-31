82ed7ed8374b8d7308371c829d89a072f83e323b
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-08-30 21:21:35 -0400

    Initial main

---
+++ Python_udemy/05_functions/06_scopes.py
@@ -0,0 +1,17 @@
+def server_chai():
+    chai_type="Masala" #local scope  
+    print(f"Inside function {chai_type}")
+chai_type="Lemon" 
+server_chai()
+print(f"Outside function {chai_type}")
+
+def chai_counter():
+    chai_order="lemon" #enclosing scope
+    def print_order():
+        chai_order="ginger" #local scope
+        print("Inner:",chai_order)
+    print_order()
+    print("Outer:",chai_order)
+chai_order="Tulsi" #global scope
+chai_counter()
+print("Global:",chai_order)
