fa2df3c7779a1b5f5525709d4af0db08561c824a
mdmustafasalman748 <mdmustafasalman748@gmail.com>
2026-08-30 21:47:38 -0400

    Initial main

---
+++ Python_udemy/05_functions/09_input_params.py
@@ -0,0 +1,31 @@
+chai="Ginger chai"
+def prepare_chai(order):
+    print("Preparing", order)
+
+prepare_chai(chai)
+print(chai)
+
+chai=[1,2,3]
+def edit_chai(cup):
+    cup[1]=42
+edit_chai(chai)
+print(chai)
+
+def make_chai(tea,milk,sugar):
+    print(tea,milk,sugar)
+
+make_chai("black","yes","low")
+
+make_chai(tea="green",sugar="medium",milk="no")
+
+def special_chai(*ingredients, **extra):
+    print("Ingredients:",ingredients)
+    print("Extra:",extra)
+    
+special_chai("Cinnamon","Cardamom",sweetner="Honey",foam="yes")
+
+def chai_order(order=[]):
+    order.append("Masala")
+    print(order)
+chai_order()
+chai_order()
